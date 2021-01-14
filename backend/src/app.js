a6738626e06e96367e8023b0e3ef7da23c169fab
Murat Erkin Çiçek <murerkinn@gmail.com>
2021-01-14 19:22:21 +0300

    Create account controller and router and implement them in app

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,9 +4,13 @@ var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
+const passport = require('passport')
 const cors = require('cors')
 
+const User = require('./models/user')
 const { mongoose } = require('./bootstrap')
+
+var accountRouter = require('./routes/account')
 
 var app = express()
 
@@ -34,10 +38,20 @@ app.use(
   })
 )
 
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(User.createStrategy())
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+
+app.use('/account', accountRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
