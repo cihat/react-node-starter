e1abac7c08ff05194942d34275a9ecb38d49acb2
Murat Erkin Çiçek <murerkinn@gmail.com>
2021-01-14 18:47:46 +0300

    Initial commit

---
+++ backend/src/app.js
@@ -0,0 +1,61 @@
+var createError = require('http-errors')
+var express = require('express')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const cors = require('cors')
+
+const { mongoose } = require('./bootstrap')
+
+var app = express()
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+)
+
+app.set('trust proxy', 1)
+
+app.use(
+  session({
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      stringify: false,
+    }),
+    secret: 'somesupersecretthing',
+    cookie: {
+      maxAge: 14 * 24 * 60 * 60 * 1000,
+      sameSite: process.env.NODE_ENV == 'production' && 'none',
+      secure: process.env.NODE_ENV == 'production',
+    },
+  })
+)
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  res.status(err.status || 500)
+  res.send(
+    req.app.get('env') === 'development'
+      ? { stack: err.stack, message: err.message }
+      : { message: err.message }
+  )
+})
+
+module.exports = app
