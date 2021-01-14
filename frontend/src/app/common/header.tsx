dcb203c160386f361a10e860875a42ea2ce1967b
Murat Erkin Çiçek <murerkinn@gmail.com>
2021-01-14 21:12:43 +0300

    Update header to conditionally render register, login, logout menu items

--- frontend/src/app/common/header.tsx
+++ frontend/src/app/common/header.tsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { logout } from '../../login/login-store'
+import { AppState } from '../store'
 
 const Header: React.FC = () => {
+  const dispatch = useDispatch()
+  const { userInfo } = useSelector((state: AppState) => state.user)
+
+  let isLoggedIn = userInfo !== null
+
+  const doLogout = () => {
+    dispatch(logout())
+  }
+
   return (
     <header className="mb-5">
       <nav className="navbar navbar-expand-lg navbar-light">
@@ -22,16 +34,27 @@ const Header: React.FC = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
             <ul className="navbar-nav mb-2 mb-lg-0" style={{ marginLeft: 'auto' }}>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">
-                  Login
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/register">
-                  Register
-                </Link>
-              </li>
+              {!isLoggedIn && (
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">
+                    Login
+                  </Link>
+                </li>
+              )}
+              {!isLoggedIn && (
+                <li className="nav-item">
+                  <Link className="nav-link" to="/register">
+                    Register
+                  </Link>
+                </li>
+              )}
+              {isLoggedIn && (
+                <li className="nav-item">
+                  <button className="nav-link btn btn-link" onClick={doLogout}>
+                    Logout
+                  </button>
+                </li>
+              )}
             </ul>
           </div>
         </div>
