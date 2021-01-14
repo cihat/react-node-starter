3616792b3a73d959a3e07043d997eca9a159a153
Murat Erkin Çiçek <murerkinn@gmail.com>
2021-01-14 20:01:03 +0300

    Implement form validation for login

--- frontend/src/login/login.tsx
+++ frontend/src/login/login.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Formik, Field, Form } from 'formik'
+import * as yup from 'yup'
 
 interface Values {
   email: string
   password: string
 }
+
+const loginSchema = yup.object().shape({
+  email: yup.string().email('Please provide a valid email address').required('Email is required'),
+  password: yup.string().required('Password is required'),
+})
 
 const Login = () => {
   const handleSubmit = ({ email, password }: Values) => {
@@ -21,30 +27,53 @@ const Login = () => {
               email: '',
               password: '',
             }}
+            validationSchema={loginSchema}
             onSubmit={values => handleSubmit(values)}
           >
-            <Form>
-              <div className="form-group mb-3">
-                <label htmlFor="email">Email</label>
-                <Field type="email" className="form-control" name="email" />
-              </div>
-              <div className="form-group mb-3">
-                <label htmlFor="password">Password</label>
-                <Field type="password" className="form-control" name="password" />
-              </div>
-              <div className="row pt-3">
-                <div className="col-8 pt-3">
-                  <Link to="/register" className="have-an-account">
-                    Don't have an account? Click here to register
-                  </Link>
+            {({ errors, touched }) => (
+              <Form>
+                <div className="form-group mb-3">
+                  <label htmlFor="email">Email</label>
+                  <Field
+                    type="email"
+                    className={[
+                      'form-control',
+                      errors.email && touched.email && 'is-invalid',
+                      !errors.email && touched.email && 'is-valid',
+                    ].join(' ')}
+                    name="email"
+                  />
+                  {errors.email && touched.email ? <div className="invalid-feedback">{errors.email}</div> : null}
                 </div>
-                <div className="col-4">
-                  <button className="btn btn-primary float-end" type="submit">
-                    Login
-                  </button>
+                <div className="form-group mb-3">
+                  <label htmlFor="password">Password</label>
+                  <Field
+                    type="password"
+                    className={[
+                      'form-control',
+                      errors.password && touched.password && 'is-invalid',
+                      !errors.password && touched.password && 'is-valid',
+                    ].join(' ')}
+                    name="password"
+                  />
+                  {errors.password && touched.password ? (
+                    <div className="invalid-feedback">{errors.password}</div>
+                  ) : null}
                 </div>
-              </div>
-            </Form>
+                <div className="row pt-3">
+                  <div className="col-8 pt-3">
+                    <Link to="/register" className="have-an-account">
+                      Don't have an account? Click here to register
+                    </Link>
+                  </div>
+                  <div className="col-4">
+                    <button className="btn btn-primary float-end" type="submit">
+                      Login
+                    </button>
+                  </div>
+                </div>
+              </Form>
+            )}
           </Formik>
         </div>
       </div>
