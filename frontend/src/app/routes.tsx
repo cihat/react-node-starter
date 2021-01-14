962515d7912ad9810d3d9d6d4fb9b108d97578cb
Murat Erkin Çiçek <murerkinn@gmail.com>
2021-01-14 21:21:27 +0300

    Implement conditional routing

--- frontend/src/app/routes.tsx
+++ frontend/src/app/routes.tsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import Header from './common/header'
 import Home from '../home/home'
 import Login from '../login/login'
 import Register from '../register/register'
+import Dashboard from '../dashboard/dashboard'
+import { AppState } from './store'
 
 const Routes = () => {
+  const { userInfo } = useSelector((state: AppState) => state.user)
+
+  let isLoggedIn = userInfo !== null
+
   return (
     <Router>
       <Header />
       <Switch>
         <Route path="/" component={Home} exact />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
+        <Route path="/login">{!isLoggedIn ? <Login /> : <Redirect to="/dashboard" />}</Route>
+        <Route path="/register">{!isLoggedIn ? <Register /> : <Redirect to="/dashboard" />}</Route>
+        <Route path="/dashboard">{isLoggedIn ? <Dashboard /> : <Redirect to="/login" />}</Route>
       </Switch>
     </Router>
   )
